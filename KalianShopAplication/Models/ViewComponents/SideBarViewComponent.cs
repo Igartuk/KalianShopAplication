0907c510ab5032f12b22de14d14427a9fa1a3d1f
DESKTOP-SPDNOK0\igor <Igartuk22@yandex.by>
2021-09-24 05:46:55 +0300

    feat: add Services edition,page Services, set up footer,header,add sidebar,

---
+++ KalianShopAplication/Models/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using KalianShopApplication.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalianShopApplication.Models.ViewComponents
+{
+    public class SideBarViewComponent : ViewComponent 
+    {
+        private readonly DataManager dataManager;
+        public SideBarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
